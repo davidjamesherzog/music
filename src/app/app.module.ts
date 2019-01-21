38cefedca53b1a8a29678891e9713fa0a434027f
Dave Herzog <davidjherzog@gmail.com>
2019-01-20 20:43:55 -0500

    Implement lazy image loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+import { LazyLoadImageModule, intersectionObserverPreset } from 'ng-lazyload-image';
 import { StateModule } from './state/state.module';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
@@ -20,6 +21,9 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule,
     HttpClientModule, 
     HttpClientJsonpModule,
+    LazyLoadImageModule.forRoot({
+      preset: intersectionObserverPreset
+    }),
     StateModule
   ],
   providers: [
